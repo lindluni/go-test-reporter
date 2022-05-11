ab0f961995ade68337357c6996ed21b729ed696b
Brett Logan <lindluni@github.com>
2022-05-10 21:55:41 -0400

    Add rules variable

--- dist/index.js
+++ dist/index.js
@@ -1980,13 +1980,15 @@ const fs = __nccwpck_require__(147)
 const core = __nccwpck_require__(186)
 
 async function main() {
-    const input = core.getInput('coverage_file', { required: true, trimWhitespace: true })
+    const file = core.getInput('coverage_file', { required: true, trimWhitespace: true })
+    const rules = core.getInput('coverage_rules', { required: true, trimWhitespace: true })
     const coverage = core.getInput('coverage_threshold', { required: true, trimWhitespace: true })
 
-    const lines = JSON.parse(fs.readFileSync(input, 'utf8'))
+    const lines = JSON.parse(fs.readFileSync(file, 'utf8'))
     const results = [
         [{data: 'Package', header: true}, {data: 'Coverage', header: true}, {data: 'Result', header: true}],
     ]
+
     for(const line of lines ){
         if(line.Output && line.Output.startsWith('coverage')){
             const coverage_value = parseFloat(line.Output.split(' ')[1].split('%')[0])
@@ -2002,7 +2004,7 @@ async function main() {
     await core.summary
         .addHeading('Code Coverage Report')
         .addTable(results)
-        .addLink('Code Coverage Rules', 'https://github.com/actions/code-coverage-rules')
+        .addLink('Code Coverage Guidelines', rules)
         .write()
 }
 
--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const fs = require('fs')
 const core = require('@actions/core')
 
 async function main() {
-    const input = core.getInput('coverage_file', { required: true, trimWhitespace: true })
+    const file = core.getInput('coverage_file', { required: true, trimWhitespace: true })
+    const rules = core.getInput('coverage_rules', { required: true, trimWhitespace: true })
     const coverage = core.getInput('coverage_threshold', { required: true, trimWhitespace: true })
 
-    const lines = JSON.parse(fs.readFileSync(input, 'utf8'))
+    const lines = JSON.parse(fs.readFileSync(file, 'utf8'))
     const results = [
         [{data: 'Package', header: true}, {data: 'Coverage', header: true}, {data: 'Result', header: true}],
     ]
+
     for(const line of lines ){
         if(line.Output && line.Output.startsWith('coverage')){
             const coverage_value = parseFloat(line.Output.split(' ')[1].split('%')[0])
@@ -24,7 +26,7 @@ async function main() {
     await core.summary
         .addHeading('Code Coverage Report')
         .addTable(results)
-        .addLink('Code Coverage Rules', 'https://github.com/actions/code-coverage-rules')
+        .addLink('Code Coverage Guidelines', rules)
         .write()
 }
 
